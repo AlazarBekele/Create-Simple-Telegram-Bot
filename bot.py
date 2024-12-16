f991b33ec5678f287ecf97b55d1b8bb5b077e5e8
AlazarBekele <alazarthe70@gmail.com>
2024-12-16 19:39:55 +0300

    Create .env & Bot.py install pyTelegramBotAPI

--- bot.py
+++ bot.py
@@ -1,5 +1,12 @@
-from typing import final
+import os
+
+import telebot
+
+BOT_TOKEN = os.environ.get ('7522950368:AAFhmUZAhZ9XCK_fJ_0leLSlee1IHqoi5TE')
+
+bot = telebot.TeleBot (BOT_TOKEN)
 
 
-TOKEN : final = ('7522950368:AAFhmUZAhZ9XCK_fJ_0leLSlee1IHqoi5TE')
-BOT_USERNAME : Final = ('@CodeWithAlazarBot')
+@bot.message_handler(commands=['Start' ,'Hello User'])
+def Send_Welcome_message (message):
+  bot.reply_to (message, 'Ho! Hello, How are you?')
