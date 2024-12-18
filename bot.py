1b448867c5e5e5e4b6008f6e040ba073b1cb8e69
AlazarBekele <alazarthe70@gmail.com>
2024-12-18 20:54:17 +0300

    Display The first conversation

--- bot.py
+++ bot.py
@@ -1,2 +1,11 @@
 import os
 import telebot
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler (commands=['greet'])
+def greet (message):
+  bot.reply_to(message, 'hello')
+
+bot.polling() # keep checking for message
