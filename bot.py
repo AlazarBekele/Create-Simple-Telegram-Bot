2d9c62c88e3564243461be59cbeaaa873c1cc7c5
AlazarBekele <alazarthe70@gmail.com>
2024-12-19 20:04:04 +0300

    cancel the reply as message just sent the his own message

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 import os
 import telebot
 
+
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler (commands=['greet'])
 def greet (message):
-  bot.reply_to(message, 'hello')
+  bot.reply_to(message, 'Hey! hello how are you?')
+
 
 @bot.message_handler (commands=['hello'])
 def hello (message):
-  bot.send_message (message, 'Hello!')
+  bot.send_message (message.chat.id, 'Hello!')
 
 bot.polling() # keep checking for message
